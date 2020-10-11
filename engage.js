23e8da586bd5caf9f11a969ec3747e41d18addd9
shpeacock <spencerpeacock@gmail.com>
2020-10-11 12:26:46 -0500

    needed that extra space

--- engage.js
+++ engage.js
@@ -13,7 +13,7 @@ function searchIt() {
             var newFan = data.statuses[Math.floor(Math.random() * data.statuses.length)];
             var statusObj = {
                 status: "@" + newFan.user.screen_name +
-                    "All I heard was the words trump and dick so I thought you might wanna follow my page! I feed that man dicks everyday",
+                    " All I heard was the words trump and dick so I thought you might wanna follow my page! I feed that man dicks everyday",
                 in_reply_to_status_id: newFan.id_str
             }
             whChef.post('statuses/update', statusObj);
