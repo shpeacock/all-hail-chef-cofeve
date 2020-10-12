039d5157d760edec8869304473e63bb0af4f1d13
shpeacock <spencerpeacock@gmail.com>
2020-10-11 23:48:36 -0500

    updating meals with variety

--- data/meals.js
+++ data/meals.js
@@ -1,11 +1,13 @@
 module.exports.breakfast = [
-    "biscuit sandwiches",
-    "bagel sandwiches",
-    "breakfast tacos",
-    "breakfast burritos",
-    "crepes",
-    "crossan'Wiches",
-    "omlettes"
+    " biscuit sandwiches",
+    " bagel sandwiches",
+    " breakfast tacos",
+    " breakfast burritos",
+    " crepes",
+    " crossan'Wiches",
+    " omlettes",
+    "s with breakfast potatoes and sunny d",
+    "s with grits and I tried sneaking in some pureed cauliflower",
 ]
 
 module.exports.drinkTemp = [
