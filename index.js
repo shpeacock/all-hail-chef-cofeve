b0ac959067182adf79892a5afcf02b8f4be9c9a4
shpeacock <spencerpeacock@gmail.com>
2020-10-16 20:50:57 -0500

    removed conditional responding since it was acting a fool

--- index.js
+++ index.js
@@ -1,19 +1,12 @@
 const twit = require('twit');
 const chefConfig = require('./chef.js');
 const whChef = new twit(chefConfig);
-const myid_str = [925160703006330880, 22831053, 1096867112029691911, ];
+// const myid_str = [925160703006330880, 22831053, 1096867112029691911, ];
 
 var stream = whChef.stream('statuses/filter', { track: '@realwhChef' });
 stream.on('tweet', function(tweet) {
-    if (myid_str.indexOf(tweet.user.id_str) > 0) {
-        whChef.post('statuses/update', {
-            status: "sup @" + tweet.user.screen_name + " I appreciate the good vibes!",
-            in_reply_to_status_id: tweet.id_str
-        });
-    } else {
-        whChef.post('statuses/update', {
-            status: "@" + tweet.user.screen_name + " Sigh. yes master ",
-            in_reply_to_status_id: " " + tweet.id_str
-        });
-    }
+    whChef.post('statuses/update', {
+        status: "sup @" + tweet.user.screen_name + " I appreciate the good vibes!",
+        in_reply_to_status_id: tweet.id_str
+    });
 });
