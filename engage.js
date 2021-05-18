66e6e0aa94fe75c6cfbbd373edc236f9f2125751
shpeacock <spencerpeacock@gmail.com>
2021-05-17 21:47:01 -0500

    updating the engage tweet

--- engage.js
+++ engage.js
@@ -3,7 +3,7 @@ const chefConfig = require('./chef.js');
 const whChef = new twit(chefConfig);
 
 function searchIt() {
-    whChef.get('search/tweets', { q: "\" Nunes Dick\"", }, gotData);
+    whChef.get('search/tweets', { q: "\"@DevinNunes Dick\"", }, gotData);
 
     function gotData(err, data) {
         if (!data) {
@@ -13,7 +13,7 @@ function searchIt() {
             var newFan = data.statuses[Math.floor(Math.random() * data.statuses.length)];
             var statusObj = {
                 status: "@" + newFan.user.screen_name +
-                    " All I heard was the words Nunes and dick so I thought you might wanna follow my page! I feed that man dicks everyday",
+                    " All I heard were the words Nunes and dick. I think I have something to contribute to those types of convos. I feed him dicks everyday. ",
                 in_reply_to_status_id: newFan.id_str
             }
             whChef.post('statuses/update', statusObj);
