ccd9c5ff48148520769953140a5df53261498480
shpeacock <spencerpeacock@gmail.com>
2021-05-17 19:49:04 -0500

    switching to nunes

--- breakfast.js
+++ breakfast.js
@@ -1,7 +1,6 @@
 const animals = require('animals');
 const m = require('./data/meals.js');
 const bf = m.breakfast;
-const cr = m.cereal;
 const dr = m.drink;
 const twit = require('twit');
 //these are depdedencies established in other files
@@ -13,12 +12,9 @@ function breakFast() {
     var dick = animals();
     var cooked = m.cookType[Math.floor(Math.random() * m.breakfast.length)];
     var breakfast = bf[Math.floor(Math.random() * bf.length)];
-    var cereal = cr[Math.floor(Math.random() * cr.length)];
-    var drink = dr[Math.floor(Math.random() * dr.length)];
     const params = {
-        status: 'today for breakfast @realdonaldtrump, @jaredkushner and @DonaldJTrumpJr are having' +
-            breakfast + ' with ' + cooked + dick + '-dicks.' +
-            ' @EricTrump is having a bowl of ' + cereal + ' & a sippy cup of ' + drink
+        status: 'today for breakfast @DevinNunes is having' +
+            breakfast + ' with ' + cooked + dick + '-dicks.'
     }
     whChef.post('statuses/update', params);
 }
--- engage.js
+++ engage.js
@@ -3,7 +3,7 @@ const chefConfig = require('./chef.js');
 const whChef = new twit(chefConfig);
 
 function searchIt() {
-    whChef.get('search/tweets', { q: "\" Trump Dick\"", }, gotData);
+    whChef.get('search/tweets', { q: "\" Nunes Dick\"", }, gotData);
 
     function gotData(err, data) {
         if (!data) {
@@ -13,7 +13,7 @@ function searchIt() {
             var newFan = data.statuses[Math.floor(Math.random() * data.statuses.length)];
             var statusObj = {
                 status: "@" + newFan.user.screen_name +
-                    " All I heard was the words trump and dick so I thought you might wanna follow my page! I feed that man dicks everyday",
+                    " All I heard was the words Nunes and dick so I thought you might wanna follow my page! I feed that man dicks everyday",
                 in_reply_to_status_id: newFan.id_str
             }
             whChef.post('statuses/update', statusObj);
