db8f195aea6a307777dac156d5d5216936e525fc
shpeacock <spencerpeacock@gmail.com>
2021-05-17 21:30:14 -0500

    fdsjljk

--- breakfast.js
+++ breakfast.js
@@ -1,10 +1,10 @@
 const animals = require('animals');
 const m = require('./data/meals.js');
-const bf = m.breakfast;
-<<<<<<< HEAD
-const dr = m.drink;
-=======
->>>>>>> 51058d29384df9ad0558013c2b62a7f04e4b9c38
+const bf = m.breakfast; <<
+<< << < HEAD
+const dr = m.drink; ===
+=== = >>>
+>>> > 51058 d29384df9ad0558013c2b62a7f04e4b9c38
 const twit = require('twit');
 //these are depdedencies established in other files
 //this is creating a twit object using API tokens established in a file that is ignored from git
@@ -16,13 +16,8 @@ function breakFast() {
     var cooked = m.cookType[Math.floor(Math.random() * m.breakfast.length)];
     var breakfast = bf[Math.floor(Math.random() * bf.length)];
     const params = {
-<<<<<<< HEAD
         status: 'today for breakfast @DevinNunes is having' +
             breakfast + ' with ' + cooked + dick + '-dicks.'
-=======
-        status: 'for his VERY LAST BREAKFAST in the WhiteHouse @realdonaldtrump is having' +
-            breakfast + ' with ' + cooked + dick + '-dicks.' +
->>>>>>> 51058d29384df9ad0558013c2b62a7f04e4b9c38
     }
     whChef.post('statuses/update', params);
 }
