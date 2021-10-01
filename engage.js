6b0db79c3bb9da9bbb477da2beaac53bfed58871
Spence Peacock <spence@pop-os.localdomain>
2021-10-01 17:28:01 -0500

    verbiage more

--- engage.js
+++ engage.js
@@ -14,7 +14,7 @@ function searchIt(target) {
             var newFan = data.statuses[Math.floor(Math.random() * data.statuses.length)];
             var statusObj = {
                 status: "@" + newFan.user.screen_name +
-                    " All I heard were the words Nunes and dick. I think I have something to contribute to those types of convos. I feed him dicks everyday. ",
+                    " All I heard were the words Nunes and dick. I think I have something to contribute to those types of convos. I feed him dicks quite often. ",
                 in_reply_to_status_id: newFan.id_str
             }
             whChef.post('statuses/update', statusObj);
